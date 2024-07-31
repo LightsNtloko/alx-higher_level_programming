e8d3f9e41d97ff7e8d5ccdcd2dde0433acf27bc6
LightsNtloko <ST10125711@rcconnect.edu.za>
2024-07-31 10:21:36 -0700

    A script that prints the number of movies where the character “Wedge Antilles” is present

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,36 +1,28 @@
 #!/usr/bin/node
 
+// Import the request module
 const request = require('request');
 
-// Get the API URL from command line arguments
+// Get the API URL from the command line arguments
 const apiUrl = process.argv[2];
 
-// Check if the URL is provided
-if (!apiUrl) {
-  console.error('Usage: ./4-starwars_count.js <API_URL>');
-  process.exit(1);
-}
+// Wedge Antilles character ID
+const wedgeId = 18;
 
-// Define the URL for Wedge Antilles' data
-const wedgeUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
-
-// Function to count the number of films
-const countFilms = (url) => {
-  request(url, { json: true }, (err, res, body) => {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    
-    // Check if the API response contains films
-    if (body && body.films) {
-      console.log(body.films.length);
-    } else {
-      console.error('Error: No films found for the character.');
-      process.exit(1);
-    }
-  });
-};
-
-// Call the function with the URL
-countFilms(wedgeUrl);
+// Make a GET request to the API URL
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('error:', error);
+  } else if (response.statusCode === 200) {
+    const data = JSON.parse(body);
+    let count = 0;
+    data.results.forEach(film => {
+      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${wedgeId}/`)) {
+        count++;
+      }
+    });
+    console.log(count);
+  } else {
+    console.error('Error:', response.statusCode);
+  }
+});
